254d0486af5b165fa0f9c3235506336935ad3f29
Justin Reina <justinmreina@gmail.com>
2025-03-22 09:33:09 -0700

    (C)    style revisions

--- main/main.c
+++ main/main.c
@@ -1,17 +1,23 @@
 /**************************************************************************************************/
-/** @file     station_example_main.c
+/** @file     main.c
  *  @brief    WiFi station Example
- *  @details  This example code is in the Public Domain (or CC0 licensed, at your option.)
+ *  @details  x
  *
  *  @author   Justin Reina, Firmware Engineer
- *  @created  1/15/25
- *  @last rev 1/15/25
+ *  @created  3/21/25
+ *  @last rev 3/21/25
  *
  *
- *  @notes    x
+ *  @notes    Work in progress!!
  *
  *  @section    Opens
- *      none current
+ *      unroll commands
+ *		step in the arduino code
+ *		port it over
+ *		make it work
+ *
+ *	@section  	Console Configuration
+ *		COM4 @ 115200 bps, 8 data bits, 1 stop bit, no parity & no flow ctrl
  *
  *  @section    Legal Disclaimer
  *      © 2025 Justin Reina, All rights reserved. All contents of this source file and/or any other
@@ -19,7 +25,6 @@
  *      Do not copy.
  */
 /**************************************************************************************************/
-
 
 //************************************************************************************************//
 //                                            INCLUDES                                            //
@@ -136,11 +141,17 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
 /** @fcn        void wifi_init_sta(void)
  *  @brief      x
  *  @details    x
+ *
+ *	@section  	Opens
+ *		Unroll
+ *		...!
  */
 /**************************************************************************************************/
 void wifi_init_sta(void) {
 	
+	//Locals
     s_wifi_event_group = xEventGroupCreate();
+
 
     ESP_ERROR_CHECK(esp_netif_init());
 
@@ -152,11 +163,13 @@ void wifi_init_sta(void) {
 
     esp_event_handler_instance_t instance_any_id;
     esp_event_handler_instance_t instance_got_ip;
+    
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
                                                         &event_handler,
                                                         NULL,
                                                         &instance_any_id));
+                                                        
     ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
                                                         IP_EVENT_STA_GOT_IP,
                                                         &event_handler,
@@ -165,36 +178,37 @@ void wifi_init_sta(void) {
 
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .ssid     = EXAMPLE_ESP_WIFI_SSID,
             .password = EXAMPLE_ESP_WIFI_PASS,
-            /* Authmode threshold resets to WPA2 as default if password matches WPA2 standards (password len => 8).
-             * If you want to connect the device to deprecated WEP/WPA networks, Please set the threshold value
-             * to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK and set the password with length and format matching to
-             * WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK standards.
+            /* Authmode threshold resets to WPA2 as default if password matches WPA2 standards 
+             * (password len => 8). If you want to connect the device to depr WEP/WPA networks, 
+             * Please set the threshold value to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK and set 
+             * the pswd w\length and format matching to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK standards
              */
             .threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,
-            .sae_pwe_h2e = ESP_WIFI_SAE_MODE,
+            .sae_pwe_h2e        = ESP_WIFI_SAE_MODE,
             .sae_h2e_identifier = EXAMPLE_H2E_IDENTIFIER,
         },
     };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
     
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     
     ESP_ERROR_CHECK(esp_wifi_start() );
 
     ESP_LOGI(TAG, "wifi_init_sta finished.");
 
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed 
+     * for the max # of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
     EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
 							               WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
 								           pdFALSE,
 								           pdFALSE,
 								           portMAX_DELAY);
 
-    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-     * happened. */
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which 
+     * event actually happened */
     if (bits & WIFI_CONNECTED_BIT) {
         ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
     } else if (bits & WIFI_FAIL_BIT) {
@@ -217,13 +231,18 @@ void app_main(void) {
 	
     //Initialize NVS
     esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    
+    if((ret == ESP_ERR_NVS_NO_FREE_PAGES) || (ret == ESP_ERR_NVS_NEW_VERSION_FOUND)) {
+		
       ESP_ERROR_CHECK(nvs_flash_erase());
+      
       ret = nvs_flash_init();
     }
+    
     ESP_ERROR_CHECK(ret);
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    
     wifi_init_sta();
     
     return;
